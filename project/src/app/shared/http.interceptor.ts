4529831171a88abed1bf560cadd48541a6af41fd
Supran <supsng01@in.ibm.com>
2018-07-05 18:26:51 +0530

    updated

--- project/src/app/shared/http.interceptor.ts
+++ project/src/app/shared/http.interceptor.ts
@@ -5,18 +5,21 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 
 import * as fromApp from '../store/app.reducer';
 import * as fromAuth from '../auth/store/auth.reducer';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class HttpAuthInterceptor implements HttpInterceptor{
     
     public constructor(private store:Store<fromApp.AppState>){}
-
+    
     intercept(req:HttpRequest<any> , next:HttpHandler) : Observable<HttpEvent<any>>{
-        return this.store.select('auth').take(1).map((state:fromAuth.State) => {
+      const baseUrl = environment.baseUrl;
+      console.log("Base url ==== ",baseUrl);
+      return this.store.select('auth').take(1).map((state:fromAuth.State) => {
               return state.token;
         }).switchMap(token => {
    
-              const clonedReq = req.clone({url:req.url+".json",params : req.params.set('auth',token)});
+              const clonedReq = req.clone({url:`${baseUrl}${req.url}.json`,params : req.params.set('auth',token)});
               
               return next.handle(clonedReq);
         })
--- project/src/environments/environment.prod.ts
+++ project/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl : "https://triporg-1508486982436.firebaseio.com"
 };
